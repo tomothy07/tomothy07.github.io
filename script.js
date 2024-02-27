2d512cfd9c2c078d0a1a8ddfd1a26371ab023674
Abyss-07 <130120227+Abyss-07@users.noreply.github.com>
2024-02-27 18:55:06 +1100

    Updates

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
     var buttons = document.querySelectorAll('.scrollButton');
     buttons.forEach(function(button) {
@@ -15,3 +13,27 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+var scrollToTopBtn = document.getElementById("scrollToTop");
+
+// Add a click event listener to the button
+scrollToTopBtn.addEventListener("click", function() {
+    // Scroll to the top of the page smoothly
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Smooth scrolling behavior
+    });
+});
