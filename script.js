12a34644506a4c8a8d014d45bb70e8550ef77eb6
Ethan Mole <efmol100@gmail.com>
2024-03-17 15:38:33 +1100

    split webpage into two seperate pages

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 
-
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
